93b63bf34cbb025a2651799459a24967bf5aaa8c
Pranav Bansal <pb719@ic.ac.uk>
2020-04-21 19:52:24 +0530

    Updated Events Page to show and add events

--- frontend/src/pages/Auth-Page/Auth.js
+++ frontend/src/pages/Auth-Page/Auth.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 
-import './Auth.css';
 import AuthContext from '../../context/auth-context';
 
+
+import './Auth.css';
 
 class AuthPage extends Component {
   state = {
@@ -14,8 +15,8 @@ class AuthPage extends Component {
 
   constructor(props) {
     super(props);
-    this.emailEl = React.createRef();
-    this.passwordEl = React.createRef();
+    this.eamilElRef = React.createRef();
+    this.passwordElRef = React.createRef();
   }
 
   switchModeHandler = () => {
@@ -26,8 +27,8 @@ class AuthPage extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    const email = this.emailEl.current.value;
-    const password = this.passwordEl.current.value;
+    const email = this.eamilElRef.current.value;
+    const password = this.passwordElRef.current.value;
 
     // Validating the credentials
     if(email.trim().length === 0 || password.trim().length === 0) {
@@ -91,12 +92,12 @@ class AuthPage extends Component {
     return <form className="auth-form" onSubmit={this.submitHandler}> 
       <div className="form-control">
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" ref={this.emailEl} />
+        <input type="email" id="email" ref={this.eamilElRef} />
       </div>
 
       <div className="form-control">
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" ref={this.passwordEl} />
+        <input type="password" id="password" ref={this.passwordElRef} />
       </div>
 
       <div className="form-action">
--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -4,44 +4,43 @@ const { transformEvent } = require('../../helpers/resolver_helpers')
 
 
 module.exports = {
-    events: async () => {
-        try{
-            const events = await Event.find()
-            return events.map(event => {
-            return transformEvent(event);
-            })
-        } catch (err) {
-            throw err;
-        }
-    }, 
-    createEvent: async (args, req) => {
-        // Authentticating the request
-        if(!req.isAuth) {
-            throw new Error('Unauthenticated access');
-        }
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async (args, req) => {
+    if (!req.isAuth) {
+      throw new Error('Unauthenticated!');
+    }
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: req.userId
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById(req.userId);
 
+      if (!creator) {
+        throw new Error('User not found.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
 
-        const event = new Event({
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: new Date(args.eventInput.date),
-            creator: req.userid  
-        });  
-        let createdEvent;
-        try {
-            const result = await event.save();
-            createdEvent = transformEvent(result);
-            const creator = await User.findById("5e965d0dd101182163b8819d");
-            if (!creator) {
-                throw new Error('No such User exists');
-            }
-            creator.createdEvents.push(event);
-            return createdEvent;
-        }
-        catch (err) {
-            console.log(err);
-            throw err;
-        }
-    },
-}
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};
